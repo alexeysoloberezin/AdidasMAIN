d357c1bb354f3a61913bcf8373b0b0f3c8059e6b
alexeysoloberezin <71139559+alexeysoloberezin@users.noreply.github.com>
2020-09-15 10:34:09 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,10 +4,8 @@ $(document).ready(function () {
         swipe: false,
     });
 });
-
 $('.burger').click(function () {
-    $('.header__row').toggleClass('_active-menu');
-   
+    $('.header__row').toggleClass('_active-menu');   
     $('.burger').toggleClass('_active-burger');
 });
 
@@ -19,6 +17,7 @@ $('.name__size').click(function () {
     $('.name__btn').toggleClass('_btn-anim');
 });
 
+
 // ADD TO CART===========================================================================
 let sliderOne = $('.slider-one');
 let sliderTwo = $('.slider-two');
@@ -29,53 +28,58 @@ let mainSlider = $('.main__slider');
 
 $(document).click(function(){
     if (sliderOne.hasClass('slick-active')){
-        replaceCash('120');
-        replaceName('superstar');
+        $('.cash').text('120');        
+        $('.name__titile').text('superstar');
+        $('.color__circle').removeClass('active-color');
+        $('.color__circle1').addClass('active-color'); 
     }
     if (sliderTwo.hasClass('slick-active')){
-        replaceCash('139');
-        replaceName('StreatBall');
+        $('.cash').text('139');
+        $('.name__titile').text('StreatBall');  
+        $('.color__circle').removeClass('active-color');
+        $('.color__circle2').addClass('active-color');      
     }    
     if (sliderThree.hasClass('slick-active')){
-        replaceCash('149');
-        replaceName('Hoops');        
+        $('.cash').text('149');             
+        $('.name__titile').text('Hoops'); 
+        $('.color__circle').removeClass('active-color');
+        $('.color__circle3').addClass('active-color'); 
     }
     if (sliderFor.hasClass('slick-active')){
-        replaceCash('169');
-        replaceName('Ozwego');
+        $('.cash').text('169');
+        $('.name__titile').text('Ozwego'); 
+        $('.color__circle').removeClass('active-color');
+        $('.color__circle4').addClass('active-color');        
     }
     if (sliderFive.hasClass('slick-active')){
-        replaceCash('219');
-        replaceName('Sonkei');
-    }    
+        $('.name__titile').text('Sonkei');
+        $('.cash').text('219');    
+        $('.color__circle').removeClass('active-color');
+        $('.color__circle5').addClass('active-color');   
+    }           
 })
+
 $('.name__item1').click(function(){
-    $('.name__size').text('37');
-});
+    $('.name__size').text('37');    
+})
 $('.name__item2').click(function(){
     $('.name__size').text('38');
-});
+})
 $('.name__item3').click(function(){
     $('.name__size').text('39');
-});
+})
 $('.name__item4').click(function(){
     $('.name__size').text('40');
-});
+})
 $('.name__item5').click(function(){
     $('.name__size').text('41');
-});
+})
 $('.name__item6').click(function(){
     $('.name__size').text('42');
-});
+})
 $('.name__item7').click(function(){
     $('.name__size').text('43');
-});
+})
 $('.name__item8').click(function(){
     $('.name__size').text('44');
-});
-function replaceCash(item){
-    $('.cash').text(`${item}`);
-}
-function replaceName(name){
-    $('.name__titile').text(`${name}`);
-}
+})
